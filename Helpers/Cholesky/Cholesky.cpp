7569cf5bfdc2269e1a0b6a26e79e25244b178462
Edouard <edouard.laferte@orange.fr>
2025-08-30 12:02:57 -0400

    update_2

---
+++ Helpers/Cholesky/Cholesky.cpp
@@ -0,0 +1,13 @@
+#include "Cholesky.hpp"
+
+Cholesky::Cholesky(){
+
+}
+
+Cholesky::Cholesky(double x):x_(x){
+    
+}
+
+double Cholesky::test(){
+    return x_;
+}
---
+++ Helpers/Cholesky/Cholesky.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+
+class Cholesky{
+
+public:
+
+    Cholesky();
+    Cholesky(double x);
+
+    double test();
+
+private:
+    double x_;
+};
