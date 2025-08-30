b25c06a54bf31ac2200ee45e6f5ff9dbad3ae709
Edouard <edouard.laferte@orange.fr>
2025-08-30 11:35:07 -0400

    cpp_run_file

---
+++ Numerical_Algebra/HW1/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    std::cout << "I am happy code in Cpp!\n";
+    return 0;
+}
+
+// To run it:
+
+// open terminal at the root of the repository (got there using cd)
+// Then cd Numerical_Algebra and cd HW1
+// run make everytime you do and udpate to the code
+// run ./main to run the main.cpp
+
+// If you are not coding with makefile then god bless you
+
+// We can create a dll if needed to see the results in python or use cython I guess
+
+
+
